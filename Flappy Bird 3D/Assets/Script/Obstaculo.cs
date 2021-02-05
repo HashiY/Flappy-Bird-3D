f1ff4ce8f62e8ca086851a4ba967c283c754b72b
HashiY <47865897+HashiY@users.noreply.github.com>
2021-02-05 18:43:39 -0300

    up

---
+++ Flappy Bird 3D/Assets/Script/Obstaculo.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Obstaculo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        this.transform.position = new Vector3(this.transform.position.x,this.transform.position.y,10); // eu - começa nessa posiçao
+		GetComponent<Rigidbody>().velocity = new Vector3(0.0f, 0.0f, -5.0f); //  andar
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(this.transform.position.z < -20){ // quebra se chegar aqui
+			Destroy(this.gameObject);
+		}
+	}
+}
